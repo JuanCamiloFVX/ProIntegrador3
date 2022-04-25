dc4f0fd63f93e675e808805290a64a2ef19300d6
Harinton <harintond.arizav@unac.edu.co>
2022-04-25 15:02:38 -0500

    List Reports and Animatables

--- screens/Page1.js
+++ screens/Page1.js
@@ -1,51 +1,102 @@
-import { View, Text, StyleSheet } from "react-native";
-import React from "react";
-import ReportList from "../components/ReportList/ReportList";
-import { Button} from "react-native-paper";
-import * as Animatable from 'react-native-animatable';
+import { View, Text, StyleSheet, ScrollView, SafeAreaView } from 'react-native'
+import React from 'react'
+import ReportList from '../components/ReportList/ReportList'
+import { Button, Title } from 'react-native-paper'
+import * as Animatable from 'react-native-animatable'
 
 export default function Page1({ navigation }) {
   return (
-    <Animatable.View
-    animation="fadeInUpBig"
-    >
-    <View>
-      <ReportList />
-      <View style= {styles.button1}>
-        <Button
-          icon="upload"
-          mode="contained"
-          color="#253659"
-          onPress={() => console.log("Pressed")}
-        >
-         Nuevo Reporte
-        </Button>
-              <View style= {styles.button2}>
-        <Button
-          icon="signal-cellular-3"
-          mode="contained"
-          color="#F27457"
-          onPress={() => console.log("Pressed")}
-        >
-         Mostrar Grafica
-        </Button>
+    <SafeAreaView style={styles.safe}>
+    <ScrollView style={styles.scroll}>
+      <View style={styles.container}>
+      <Animatable.View animation="fadeInUpBig">
+        <View>
+          <View style={styles.title_container}>
+            <Title style={styles.title}>Gestiona tus Reportes</Title>
+          </View>
+          <View style={styles.report_container}>
+          <ReportList />
+          </View>
+        </View>
+      </Animatable.View>
       </View>
-      </View>
-    </View>
-    </Animatable.View>
-  );
+    </ScrollView>
+    <Animatable.View animation="fadeInLeft">
+    <View style={styles.container_buttons}>
+            <Button
+              style={styles.button1}
+              icon="upload"
+              mode="contained"
+              color="#03A696"
+              onPress={() => console.log('Pressed')}
+            >
+              Nuevo Reporte
+            </Button>
+            <Button
+              style={styles.button2}
+              icon="signal-cellular-3"
+              mode="contained"
+              color="#03A696"
+              onPress={() => console.log('Pressed')}
+            >
+              Mostrar Grafica
+            </Button>
+           
+          </View>
+          </Animatable.View>
+    </SafeAreaView>
+  )
 }
 const styles = StyleSheet.create({
-  button1:{
-    paddingTop:20,
-    left:10,
-    width:180, 
-    marginTop:10
+  safe:{
+    backgroundColor:"#253659",
+    height:"100%"
   },
-  button2:{
-    paddingTop:20,
-    position:"absolute",
-    width:180, 
-    left:190
-  }
-});
+  scroll: {
+    backgroundColor:"#253659",
+    height:"92%"
+
+  },
+  container_buttons: {
+    display: 'flex',
+    width: '100%',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    paddingLeft: "3%",
+    paddingRight: "3%",
+    marginTop:10,
+    marginBottom:20
+  },
+  button1: {
+    width: 180,
+    height: 40,
+  },
+  button2: {
+    width: 180,
+    height: 40,
+  },
+  title:{
+    marginTop:10,
+    fontSize:30,
+    color:"#fff",
+    textAlign:'center',
+    fontWeight:"bold"
+  }, 
+  title_container:{
+    marginTop:10,
+    marginBottom:10,
+    backgroundColor:"#04bf9d", 
+    height:50,
+    borderRadius:50,
+    marginLeft:"2%",
+    marginRight:"2%"
+  },
+report_container:{
+  borderRadius:20,
+  backgroundColor:"#fff",
+  marginLeft:"2%",
+  marginRight:"2%"
+}, 
+container:{
+},
+})
